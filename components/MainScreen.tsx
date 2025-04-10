29ff7e86c3156736e73df1c5b08dd23971448b7e
Makomori37 <oun_kup@hotmail.com>
2025-04-10 13:23:27 +0700

    style title

--- components/MainScreen.tsx
+++ components/MainScreen.tsx
@@ -1,13 +1,24 @@
 import { StyleSheet, View } from "react-native";
-import CircularMenu from "./widget/CircularMenu";
-import CustomText from "../utils/CustomText";
+import dayjs from "dayjs";
 
+import CircularMenu from "./widget/CircularMenu";
+
+import CustomText from "../utils/CustomText";
 import { COLORS, SIZES } from "../constants";
 
 export default function MainScreen() {
   return (
     <View style={styles.container}>
       <CustomText style={styles.title}>MAKO GAME</CustomText>
+
+      <CustomText style={styles.dateText}>
+        {dayjs().format("dddd MMMM YYYY")}
+      </CustomText>
+      <View style={styles.timeContainer}>
+        <CustomText style={styles.timeText}>
+          {dayjs().format("HH:mm")}
+        </CustomText>
+      </View>
       <CircularMenu />
     </View>
   );
@@ -21,7 +32,22 @@ const styles = StyleSheet.create({
   },
   title: {
     color: COLORS.blurPink,
-    fontSize: SIZES.xxLarge,
+    fontSize: SIZES.xLarge,
     fontWeight: "bold",
+  },
+  dateText: {
+    fontSize: 18,
+    color: COLORS.blurPink,
+    marginTop: 30,
+  },
+  timeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  timeText: {
+    fontSize: 48,
+    fontWeight: "bold",
+    color: COLORS.blurPink,
+    marginVertical: 10,
   },
 });
