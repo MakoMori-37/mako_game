ea42a15281e17aeb4e359210b1b825f66942bb78
Makomori37 <oun_kup@hotmail.com>
2025-04-10 11:54:13 +0700

    add menu item

--- components/widget/CircularMenu.tsx
+++ components/widget/CircularMenu.tsx
@@ -1,13 +1,63 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Image } from "react-native";
 
-import { COLORS, SIZES } from "../../constants";
-import CustomText from "../../utils/CustomText";
+import { COLORS, images } from "../../constants";
 import MenuItem from "./MenuItem";
 
 export default function CircularMenu() {
-  return <View style={styles.container}>{/* <MenuItem /> */}</View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.row1}>
+        <MenuItem imageLabel={images.book} textLabel="Scrabble" />
+        <MenuItem imageLabel={images.comingSoon} textLabel="coming soon" />
+      </View>
+
+      <View style={styles.row}>
+        <MenuItem imageLabel={images.comingSoon} textLabel="coming soon" />
+
+        <TouchableOpacity style={styles.fingerprintButton}>
+          <Image source={images.textGame} style={styles.image} />
+        </TouchableOpacity>
+
+        <MenuItem imageLabel={images.comingSoon} textLabel="coming soon" />
+      </View>
+
+      <View style={styles.row1}>
+        <MenuItem imageLabel={images.comingSoon} textLabel="coming soon" />
+        <MenuItem imageLabel={images.comingSoon} textLabel="coming soon" />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    marginTop: 30,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "100%",
+    marginVertical: 10,
+    alignItems: "center",
+    gap: 20,
+  },
+  row1: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "100%",
+    marginVertical: 10,
+    paddingHorizontal: 55,
+  },
+  fingerprintButton: {
+    backgroundColor: COLORS.pink,
+    padding: 20,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  image: {
+    width: 30,
+    height: 30,
+  },
 });
